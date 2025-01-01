cbb7d941ece301d87d258c0b1f77fbebd464a293
Parth Mujumdar <71146627+parthmujumdar@users.noreply.github.com>
2025-01-01 17:45:23 -0500

    Update streamlit_app.py
    Error fix of nutritional info

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,11 +41,14 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
 
-# New Section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+
+
+# New Section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
